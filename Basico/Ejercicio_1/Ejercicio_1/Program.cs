cf716108c56ab638fe02656a39b634e77f770d24
Adriel Emanuel García Penn <adrielemanuelgarciapenn@gmail.com>
2024-11-17 20:02:12 -0400

    Add files via upload
    Operaciones Basicas

---
+++ Basico/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+    class Program 
+    {
+
+    static void Main(string[] args)
+    { 
+
+Console.WriteLine("Calculadora Basica");
+Console.WriteLine();
+
+//Declaracion de variables
+double valor1, valor2, suma, resta, multiplica, divide;
+
+//Entrada de datos
+Console.Write("Introduzca el primer valor: ");
+valor1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Introduzca el segundo valor: ");
+valor2 = Convert.ToDouble(Console.ReadLine());
+
+//Proceso
+suma = valor1 + valor2;
+resta = valor1 - valor2;
+multiplica = valor1 * valor2;
+divide = valor1 / valor2;
+
+//Salida de la informacion 
+Console.WriteLine("El resultado de la suma es {0}", suma);
+Console.WriteLine("El resultado de la resta es " + resta);
+Console.WriteLine($"El resultado de la multiplicacion es {multiplica} ");
+Console.WriteLine("El resultado de la division es " + divide.ToString());
+
+
+        }
+    }
+
