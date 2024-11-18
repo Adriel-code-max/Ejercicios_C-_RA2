88b1e8d3a78d90924500610420fd0217c3575bde
Adriel Emanuel García Penn <adrielemanuelgarciapenn@gmail.com>
2024-11-17 20:03:10 -0400

    Add files via upload
    Calcular el sueldo neto de un empleado

---
+++ Basico/Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Nombre = "";
+        int tiempo;
+        double sueldo;
+        double descuentoAFP;
+        double descuentoSFS;
+        double totalDescuento;
+        double sueldoFinal;
+
+        Console.WriteLine("Nombre:");
+        Nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Sueldo:");
+        sueldo = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Tiempo:");
+        tiempo = Convert.ToInt32(Console.ReadLine());
+
+        double totalSueldo = sueldo * tiempo;
+
+        descuentoAFP = totalSueldo * 0.0287;
+        descuentoSFS = totalSueldo * 0.0304;
+        totalDescuento = descuentoAFP + descuentoSFS;
+
+        sueldoFinal = totalSueldo - totalDescuento;
+
+        Console.WriteLine($"Sueldo final del empleado {Nombre} es de {sueldoFinal}");
+        Console.ReadLine();
+    }
+}
+  
