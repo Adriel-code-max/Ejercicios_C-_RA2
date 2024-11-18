229f4a92355682a4a5b9e90b2dfd4da98599aea4
Adriel Emanuel García Penn <adrielemanuelgarciapenn@gmail.com>
2024-11-17 20:06:25 -0400

    Add files via upload
    Calcular el importe de un articulo

---
+++ Basico/Ejercicio_5/Ejercicio_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int cantidad;
+        double precio;
+        double importe;
+
+       
+        Console.Write("Ingrese la cantidad del artículo: ");
+        cantidad = Convert.ToInt32(Console.ReadLine());
+
+       
+        Console.Write("Ingrese el precio del artículo: ");
+        precio = Convert.ToDouble(Console.ReadLine());
+
+        
+        importe = cantidad * precio;
+
+       
+        Console.WriteLine("El importe del artículo es: " + importe);
+        Console.ReadLine();
+    }
+}
